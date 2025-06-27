640036945cc09eae50f2d3a230be73442e24f115
Ariane <ariane.de@outlook.com>
2025-06-27 17:59:38 -0300

    feature: Adding Bank class with operation options

---
+++ src/main/java/org/example/Bank.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Bank {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Conta c1 = new Conta(1234, 5, "Caio Lakes", "987.0125.01245-00");
+        double valor;
+        int opcao;
+
+        do {
+            System.out.println("****** YouBank *******");
+            System.out.println("1 - Extrato / 2 - Deposito / 3 - Saque / -1 - Encerrar");
+            opcao = sc.nextInt();
+            switch (opcao) {
+                case 1:
+                    System.out.println(c1.exibirInfo());
+                    break;
+                case 2:
+                    System.out.println("Digite o valor a depositar");
+                    valor = sc.nextDouble();
+                    c1.depositar(valor);
+                    break;
+                case 3:
+                    System.out.println("Digite o valor do saque");
+                    valor = sc.nextDouble();
+                    if (c1.sacar(valor)) {
+                        System.out.println("Saque autorizado");
+                    } else
+                        System.out.println("Saldo insuficiente");
+
+                case -1:
+                    System.out.println("Obrigado por utilizar nossos serviços");
+                    break;
+                default:
+                    System.out.println("Opção Invalida!");
+            }
+        }while (opcao != -1) ;
+
+            sc.close();
+
+        }
+    }
--- src/main/java/org/example/Conta.java
+++ src/main/java/org/example/Conta.java
@@ -5,6 +5,17 @@ public class Conta {
     private int digitoVerif;
     private String nomeTitular;
     private String cpfTitular;
+    private double saldo;
+
+    public Conta(int numeroConta, int digitoVerif, String nomeTitular, String cpfTitular) {
+        super();
+        this.numeroConta = numeroConta;
+        this.digitoVerif = digitoVerif;
+        this.nomeTitular = nomeTitular;
+        this.cpfTitular = cpfTitular;
+        this.saldo = 0.0;
+
+    }
 
     public int getNumeroConta() {
         return numeroConta;
@@ -38,13 +49,21 @@ public class Conta {
         this.cpfTitular = cpfTitular;
     }
 
-    public double getSaldo() {
-        return saldo;
+    public String exibirInfo(){
+        return "Conta" +numeroConta+"/"+" - "+nomeTitular+" ("+cpfTitular+" R$ "+String.format("%.2f", this.saldo);
     }
 
-    public void setSaldo(double saldo) {
-        this.saldo = saldo;
+    public void depositar(double valor) {
+        this.saldo += valor;
+    }
+    public boolean sacar(double valor){
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
     }
 
-    private double saldo;
 }
+
+
